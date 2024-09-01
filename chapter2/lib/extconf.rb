a126f48368a5f81637e8ae410c1333086bff4809
Hironobu Inatsuka <168434667+tribusonz-2@users.noreply.github.com>
2024-09-01 09:21:39 +0900

    Delete/Remove chapter2/lib directory

--- chapter2/lib/extconf.rb
+++
@@ -1,9 +0,0 @@
-require 'mkmf'
-
-if have_header('quadmath.h')
-  have_func('rb_opts_exception_p', 'ruby.h')
-  $libs << " -lquadmath"
-  create_makefile('quadmath')
-else
-  stderr.puts "quadmath not found"
-end
